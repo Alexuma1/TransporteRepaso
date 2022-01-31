0757c602350ddc3fa4846c72e9772b1f24e48d8a
alexuma <m5a97b@gmail.com>
2022-01-31 15:49:37 -0300

    diseño admin + listado de novedades

---
+++ models/novedadesModels.js
@@ -0,0 +1,9 @@
+var pool = require('./bd')
+
+async function getNovedades(){
+    var query = 'SELECT * FROM novedades'
+    var rows = await pool.query(query)
+    return rows
+}
+
+module.exports = {getNovedades}
